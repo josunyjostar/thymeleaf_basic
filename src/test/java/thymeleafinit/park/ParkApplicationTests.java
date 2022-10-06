e0f1309e06576ff30fae4b3f97f60e50ceb8ba09
parkjeyoung <jypark@newploy.net>
2022-10-06 14:49:34 +0900

    feat : spring default setting

---
+++ src/test/java/thymeleafinit/park/ParkApplicationTests.java
@@ -0,0 +1,13 @@
+package thymeleafinit.park;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ParkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
